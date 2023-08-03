59d4c1e9bcb94d223c35169738a66a622108d683
Flopiras <Flopiras@yahoo.it>
2023-08-03 16:26:56 +0200

    add navbar links

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,5 +1,17 @@
+@php     $links = config('header_links'); @endphp
+    
+
 <header>
     <div class="container">
-        <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="DC logo">
+            <img src="{{Vite::asset('resources/img/dc-logo.png')}}" alt="DC logo">
+        <nav>
+            <ul>
+                @foreach($links as $link)
+                <li>
+                    <a :href="$link['url']">{{ $link['text'] }}</a>
+                </li>
+                @endforeach
+            </ul>
+        </nav>
     </div>
 </header>
