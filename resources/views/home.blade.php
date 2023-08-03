9335cc5ee9cbe58a5c7679ddeaa390299313e854
Flopiras <Flopiras@yahoo.it>
2023-08-03 17:42:01 +0200

    add main

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,21 @@
 @extends('layouts/main')
+
+@section('title', 'Home')
+
+@section('main_content')
+
+<section id="current-series">
+    <div class="container cards-container">
+        @foreach($comics as $comic)
+        <div class="card">
+            <figure>
+                <img src="{{ $comic['thumb'] }}" :alt="{{ $comic['title'] }}">
+                <figcaption>{{ $comic['title'] }}</figcaption>
+            </figure>
+        </div>
+        @endforeach
+        <div class="button">LOAD MORE</div>
+    </div>
+</section>
+
+@endsection
