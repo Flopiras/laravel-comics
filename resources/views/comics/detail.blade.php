4674c2751774856ef44c492176d1c7b6dea38f03
Flopiras <Flopiras@yahoo.it>
2023-08-21 16:30:54 +0200

    add adv and title

--- resources/views/comics/detail.blade.php
+++ resources/views/comics/detail.blade.php
@@ -3,13 +3,27 @@
 @section('title', 'Comics | Detail')
 
 @section('main_content')
-<section id="book">
-    <div class="container">
-        <figure>
-            <p id="poster-book">COMIC BOOK</p>
-            <img id="poster" src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
-            <p id="poster-gallery">VIEW GALLERY</p>
-        </figure>
+<section id="detail">
+    <div id="book">
+        <div class="container">
+            <figure>
+                <p id="poster-book">COMIC BOOK</p>
+                <img id="poster" src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
+                <p id="poster-gallery">VIEW GALLERY</p>
+            </figure>
+        </div>
+    </div>
+    <div id="detail-main">
+        <div class="container">
+            <div id="comic-info">
+
+                <h2>{{ $comic['title'] }}</h2>
+            </div>
+            <div id="advertise">
+                <p>ADVERTISEMENT</p>
+                <img src="{{Vite::asset('resources/img/adv.jpg')}}" alt="adv">
+            </div>
+        </div>
     </div>
 </section>
 @endsection
