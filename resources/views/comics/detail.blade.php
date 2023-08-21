7b4a3026aa562f929cb8cd4f2f0b69d76106b57e
Flopiras <Flopiras@yahoo.it>
2023-08-21 16:07:58 +0200

    add book section

--- resources/views/comics/detail.blade.php
+++ resources/views/comics/detail.blade.php
@@ -3,5 +3,13 @@
 @section('title', 'Comics | Detail')
 
 @section('main_content')
-
+<section id="book">
+    <div class="container">
+        <figure>
+            <p id="poster-book">COMIC BOOK</p>
+            <img id="poster" src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
+            <p id="poster-gallery">VIEW GALLERY</p>
+        </figure>
+    </div>
+</section>
 @endsection
